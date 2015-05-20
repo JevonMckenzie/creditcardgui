75a27a9989a2e675c89267069f59ab2587b67171
Jevon Mckenzie <jevonmckenzie.wesleycollege@gmail.com>
2015-05-20 20:28:25 +0800

    required jwt, defined login_user and find_user_by_token

--- helpers/credit_card_api_helper.rb
+++ helpers/credit_card_api_helper.rb
@@ -1,7 +1,23 @@
 # helper
+require 'jwt'
 module CreditCardAPIHelper
-  def login(user)
-    session[:user_id] = user.id
+  # def login(user)
+  #   session[:user_id] = user.id
+  #   redirect '/'
+  # end
+
+  def login_user(user)
+    payload = {user_id: user.id}
+    token = JWT.encode payload, ENV['MSG_KEY'], 'HS256'
+    session[:auth_token] = token
     redirect '/'
+  end
+
+  def find_user_by_token(token)
+    return nil unless token
+    decoded_token = JWT.decode token, ENV['MSG_KEY'], true
+    payload = decoded_token.first
+    logger.info "PAYLOAD: #{payload}"
+    User.find_by_id(payload["user_id"])
   end
 end
