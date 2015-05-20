183b3d2d13672a0425aa4b158af65af8675c276b
Jevon Mckenzie <jevonmckenzie.wesleycollege@gmail.com>
2015-05-20 20:31:34 +0800

    added DB_KEY and MSG_KEY to production and development

---
+++ config/config_env.rb
@@ -0,0 +1,9 @@
+config_env :development, :test do
+  set 'DB_KEY', "Nwd8cHES5TXtyT5ORZt2jVsy20QixbNziQzmq37IXjA="
+  set 'MSG_KEY', "nOpFj9VM3XD4uYsWxnFx2HZr3MSizc_5pxmwzgy6s9s="
+end
+
+config_env :production do
+  set 'DB_KEY', "7Qbi3XJcok2WinTPRgr5t4l42L8wvrh0ElrLi5Z3qdI="
+  set 'MSG_KEY', "0pY2VsiUswep2smKNlk-kqXTqKKLwXc_1yYL5lM0TT8=" 
+end
